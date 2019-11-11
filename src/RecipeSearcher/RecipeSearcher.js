341e32b2b71c9e1f8e65139efaf5398d129fd045
Jacob Richardson <jacobrrichardson41@gmail.com>
2019-11-11 14:05:10 -0600

    Added in the other two API calls and the default state

--- src/RecipeSearcher/RecipeSearcher.js
+++ src/RecipeSearcher/RecipeSearcher.js
@@ -2,6 +2,10 @@ import React,{Component} from 'react';
 import axios from 'axios';
 
 class RecipeSearcher extends Component {
+    constructor(props){
+        super(props); 
+        this.state = { recipes: [] }
+    }
     
     getRandomRecipe = () => {
         axios ({
@@ -16,9 +20,41 @@ class RecipeSearcher extends Component {
             console.log(error);
         })
     }
+    getRecipeByName = (name) => {
+        axios 
+        ({
+            method: 'get',
+            url: 'https://www.themealdb.com/api/json/v1/1/search.php',
+            params: {
+                s:name 
+            }
+        })
+        .then(function(response){
+            console.log(response);
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    }
+    //create a function called getRecipeByLetter that takes in a letter parameter. this should carry out the proper Axios call to the database. You will need to pass in the letter as the param f in the call. 
+    getRecipesByLetter = (letter) => {
+        axios ({
+            method: 'GET',
+            url: 'https://www.themealdb.com/api/json/v1/1/search.php',
+            params: {
+                f:letter
+            }
+        })
+        .then(function(response){
+            console.log(response);
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    }
 
     render() {
-        this.getRandomRecipe();
+        this.getRecipesByLetter('C');
         return (
     <div>
 
